db521023078f10f39a0383bc0b707db46c574cbe
VereskTrusy <elpt1987@gmail.com>
2023-05-22 23:55:26 +0900

    다시보며 주석 추가함
    myproject2/src/main/java/main/web/BoardController.java :
    추가 할 수 있는 주석 추가함

--- myproject2/src/main/java/main/web/BoardController.java
+++ myproject2/src/main/java/main/web/BoardController.java
@@ -57,15 +57,17 @@ public class BoardController {
 	 */
 	@RequestMapping("/boardList.do")
 	public String selectNBoardList(BoardVO vo, ModelMap model) throws Exception {
+		// Exception 은 최대한 자세히 예상해서? 적어주는게 좋다.
+		// ex) SQLException, NullPointException.. 등등 그래야 오류났을때 구분된다함
 		
-		int unit = 5;
+		int unit = 10; // 한 페이지당 목록에 보여질 행 갯수
 		
 		int total = boardService.selectNBoardTotal(vo); // 총 데이터 개수
 		System.out.println("total : " + total);
 		// 11/10 = 1.1 -> cell(1.1) -> Integer2.0 -> 2
-		// 11/10 -> 정수타입결과는 1 -> 실수타입으로 변경해주어야
+		// 11/10 -> 정수타입결과는 1이다 -> 실수타입으로 변경해주어야
 		// 우리가 원하는 1.1이 나온다 -> (double)11/10 = 1.1
-		int totalPage = (int)Math.ceil((double)total/unit);
+		int totalPage = (int)Math.ceil((double)total/unit); 
 		System.out.println("totalPage : " + totalPage);
 		
 		int viewPage = vo.getViewPage(); // 현재 출력하려는 페이지 번호
@@ -74,26 +76,40 @@ public class BoardController {
 		// startIndex : (1-1)*10 + 1
 		// startIndex : (2-1)*10 + 1
 		
-		//
+		
+		/*
+		 * viewPage : 현재 보여지는 페이지
+		 * totalPage : 총 페이지 수
+		 * startIndex : 페이지에서 보여질 목록의 시작번호
+		 * endIndex : 페이지에서 보여질 목록의 끝번호
+		 * unit : 한 페이지당 보여질 목록의 행갯수
+		 */
+		// 사용자가 URL에 페이지넘버 임의로 입력 할 시
+		// 조치 구문
 		if(viewPage > totalPage || viewPage < 1) {
 			viewPage = 1;
 		}
 		
-		int startIndex = (viewPage-1) * unit + 1; //
-		int endIndex = startIndex + (unit-1); //
+		int startIndex = (viewPage-1) * unit + 1; // if 지금2페이지다. 시작순번은 (2-1)x10+1 = 21 부터 시작해서
+		int endIndex = startIndex + (unit-1);     // 21+(10-1) = 30 까지 목록에 보여진다
 		
-		//
-		int startRowNo =  total - ((viewPage - 1) * unit);
 		
-		vo.setStartIndex(startIndex);
-		vo.setEndIndex(endIndex);
+		int startRowNo =  total - ((viewPage - 1) * unit); // 페이지당 목록 행 시작번호 설정
+		/*
+		 * if 자료 총 55개, 보고있는 페이지 2, 한페이지당 10개씩 출력 한다고 가정 
+		 * 55 - ((2 - 1) x 10) = 45, 45 번
+		 * 부터 44, 43.. 이런식으로 행번호가 기입됨 (오래된 자료는 1번 , 신규자료는 높은 순)
+		 */
 		
-		List<?> list = boardService.selectNBoardList(vo);
+		vo.setStartIndex(startIndex); // vo 에 행 순번 세팅
+		vo.setEndIndex(endIndex);     // vo 에 행 순번 세팅
+		
+		List<?> list = boardService.selectNBoardList(vo); // DB에서 검색한 자료
 		System.out.println("list : " + list);
 		
-		model.addAttribute("rowNumber", startRowNo);
-		model.addAttribute("total", total);
-		model.addAttribute("totalPage", totalPage);
+		model.addAttribute("rowNumber", startRowNo); // 행번호가 먼저 입력되므로 먼저 보내기
+		model.addAttribute("total", total);          // 총 행 데이터 갯수
+		model.addAttribute("totalPage", totalPage);  // 페이지 총 갯수
 		model.addAttribute("resultList", list); // 받아온 list를 model에 태워서 jsp로 전송,("resultList", list) jsp에서 사용할 name, 본데이터 
 		
 		return "board/boardList";
@@ -112,7 +128,7 @@ public class BoardController {
 		BoardVO boardVO = boardService.selectNBoardDetail(vo.getUnq());
 		
 		String content = boardVO.getContent(); // \n
-		boardVO.setContent( content.replace("\n", "<br>")); // \n -> <br>
+		//boardVO.setContent( content.replace("\n", "<br>")); // \n -> <br>
 		
 		model.addAttribute("boardVO", boardVO);
 		
